7340171ca2ee06dd1b57b297a2e2f527c1315e96
Adam Staples <adam.staples@imaginelearning.com>
2020-02-06 16:38:11 -0700

    Tweaked variable usage for consistency.

--- quick-setup-config-git.sh
+++ quick-setup-config-git.sh
@@ -8,4 +8,4 @@ echo 'GIT_NAME GIT_EMAIL'
 echo '----- Configuring AWS credentials'
 git config --global credential.helper store
 git config --global user.name "${GIT_NAME}"
-git config --global user.email "$GIT_EMAIL"
+git config --global user.email "${GIT_EMAIL}"
